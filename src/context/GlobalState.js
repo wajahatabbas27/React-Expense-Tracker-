5af40f3079bdbdce72c6db5a6cb9911036d0f3f8
wajahataz <54248569+wajahatabbas27@users.noreply.github.com>
2020-12-09 20:38:57 +0500

    Update GlobalState.js

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -1,7 +1,7 @@
 import React, { createContext, useReducer } from 'react';
 
 //importing App reducer
-import {AppReducer} from './AppReducer'
+import AppReducer from './AppReducer'
 
 //initial State
 
